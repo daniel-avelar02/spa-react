52e49f394390366783eaa72dbf0fef3f8cc4c246
Daniel Avelar <82789096+DanielAvelar02@users.noreply.github.com>
2024-12-22 19:33:27 -0600

    Configuracion de ruta

--- kodigo-music/vite.config.js
+++ kodigo-music/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/spa-react>/', // Reemplaza <nombre-del-repo> con el nombre de tu repositorio
   plugins: [react()],
-})
+});
