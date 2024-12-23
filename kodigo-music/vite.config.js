62f380218233470692a2cf2d25ed6ce3fb423e65
Daniel Avelar <daniel.avelar2002@gmail.com>
2024-12-22 21:22:24 -0600

    Correcciones

--- kodigo-music/vite.config.js
+++ kodigo-music/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/spa-react/', // Reemplaza <nombre-del-repo> con el nombre de tu repositorio
   plugins: [react()],
 });
